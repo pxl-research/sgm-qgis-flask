ba79243259d2bcd01eef2a333825055fbcdfc4af
Servaas Tilkin <31237936+stilkin-pxl@users.noreply.github.com>
2023-03-31 15:38:39 +0200

    Now removes temporary files, some code clean-up

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request, send_file
 
 from forest import get_tree_rects, get_tree_img
@@ -24,17 +26,19 @@ def tree_rects():
     if result_df is not None:
         result_dict = result_df.to_dict('records')
 
-    # return as JSON
-    return jsonify(result_dict)
+    os.remove(tmp_file_name)  # remove temporary file
+    return jsonify(result_dict)  # return as JSON
 
 
 @app.route('/tree_img', methods=['POST', 'PUT'])
 def tree_img():
     print(request.files)
 
-    file = request.files['file']
-    file_name = './tmp_store.bin'
-    file.save(file_name)
+    uploaded_file = request.files['file']
+    tmp_file_name = './tmp/' + uploaded_file.filename
+    uploaded_file.save(tmp_file_name)
 
-    result_file = get_tree_img(file_name)  # perform some AI magic on this file
-    return send_file(result_file, as_attachment=True)  # send it back
+    result_file = get_tree_img(tmp_file_name)  # perform some AI magic on this file
+
+    os.remove(tmp_file_name)  # remove temporary file
+    return send_file(result_file, as_attachment=True)  # send annotated file back
